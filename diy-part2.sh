e048924c2fc63034e406de2791a1d08780b5d3dd
jieny <576420147@qq.com>
2023-05-11 14:54:27 +0800

    cr660x

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,3 +13,5 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
+# 会自己创建 package/mypackages/
+git clone https://github.com/lylus/luci-app-zerotier package/mypackages/luci-app-zerotier
